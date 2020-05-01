c1fc10b2a821ea44ab52ec2e8c5d73569e90ba3f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-05-01 14:47:52 +0530

    clickable container hover transparent removed

--- .config/awesome/widgets/clickable-container/init.lua
+++ .config/awesome/widgets/clickable-container/init.lua
@@ -12,7 +12,7 @@ function build(widget)
 	container:connect_signal(
 		'mouse::enter',
 		function()
-			container.bg = beautiful.groups_bg
+			-- container.bg = beautiful.groups_bg
 			-- Hm, no idea how to get the wibox from this signal's arguments...
 			local w = mouse.current_wibox
 			if w then
@@ -25,25 +25,11 @@ function build(widget)
 	container:connect_signal(
 		'mouse::leave',
 		function()
-			container.bg = beautiful.transparent
+			-- container.bg = beautiful.transparent
 			if old_wibox then
 				old_wibox.cursor = old_cursor
 				old_wibox = nil
 			end
-		end
-	)
-
-	container:connect_signal(
-		'button::press',
-		function()
-			container.bg = beautiful.groups_title_bg
-		end
-	)
-
-	container:connect_signal(
-		'button::release',
-		function()
-			container.bg = beautiful.groups_bg
 		end
 	)
 
