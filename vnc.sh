98ea99d4e9989dadca3b219d09b6906064700f9a
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-07-15 15:33:29 +0530

    vnc.sh updated to reflect the new IP of wu

--- vnc.sh
+++ vnc.sh
@@ -1,3 +1,3 @@
-ssh -M -L 5904:127.0.0.1:5904 -C -fNT -l deb 172.16.0.2
+ssh -M -L 5904:127.0.0.1:5904 -C -fNT -l deb 192.168.1.193
 vncviewer localhost:5904
-ssh -O exit -l deb 172.16.0.2
+ssh -O exit -l deb 192.168.1.193
