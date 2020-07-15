4bf196d747aa8ba1087792190e3cff8248778e29
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-07-15 17:00:37 +0530

    autolock added to be auto started

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -6,6 +6,7 @@ return {
     run_on_start_up = {
 	"picom --blur-method dual_kawase --blur-strength 9 --experimental-backends --config ~/.config/picom/picom.conf", 
 	"clipmenud",
+	"xautolock -detectsleep -time 5 -locker ~/.config/general/lock.sh -notify 10 -notifier \"notify-send -t 10000 -- 'LOCKING screen in 10 seconds'\"",
 --	"xinput set-prop 12 292 1",
     }
 }
