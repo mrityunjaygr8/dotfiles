eba77e22e826472dda23c7dce783cf6f2a4a8750
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-09-26 16:20:40 +0530

    new modular awesome config adopted

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -1,12 +1,24 @@
-return {
-    default = {
-        bluetooth_manager	=	"blueman-manager",
-	power_manager		= 	"xfce4-power-manager",
-    },
-    run_on_start_up = {
-	"picom --blur-method dual_kawase --blur-strength 9 --experimental-backends --config ~/.config/picom/picom.conf", 
-	"clipmenud",
-	"xautolock -detectsleep -time 5 -locker ~/.config/general/lock.sh -notify 10 -notifier \"notify-send -t 10000 -- 'LOCKING screen in 10 seconds'\"",
---	"xinput set-prop 12 292 1",
-    }
+local dpi = require("beautiful.xresources").apply_dpi
+local menubar = require("menubar")
+
+apps = {
+
+  -- Your default terminal
+  terminal        = "alacritty",
+
+  -- Your default text editor
+  editor          = os.getenv("EDITOR") or "vim",
+
+  -- editor_cmd = terminal .. " -e " .. editor,
+
+  -- Your default file explorer
+  explorer        = "pcmanfm",
+
 }
+
+apps.editor_cmd   = apps.terminal .. " -e " .. apps.editor
+apps.explorer_cmd = apps.terminal .. " -e " .. apps.explorer
+menubar.utils.terminal = apps.terminal -- Set the terminal for applications that require it
+
+
+return apps
